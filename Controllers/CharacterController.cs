d7f31f78def99df0fbf06f31fa4873d240acabbf
adrian <adrian@gmail.com>
2020-09-19 02:04:14 -0500

    git commit  -m "CharacterController"

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character {Id = 1, Name = "Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
